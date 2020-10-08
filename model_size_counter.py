44d45920c53b83f8cb10c2fec9ab904ebf0fae43
Lev Khoroshansky <sololev27@gmail.com>
2020-10-08 10:06:28 +0300

    style: more pythonic

--- model_size_counter.py
+++ model_size_counter.py
@@ -1,28 +1,24 @@
-import torch
-import torch.nn as nn
-from torch.autograd import Variable
 import numpy as np
 
+import torch
+
+from torch import nn
+from torch.autograd import Variable
+
+
 class RAMSizeCounter:
-    def __init__(self, model, input_size=(1,1,32,32), bits=32, device='cpu'):
+    def __init__(self, model, input_size=(1, 1, 32, 32), bits=32, device="cpu"):
         self.model = model
         self.input_size = input_size
         self.bits = bits
         self.device = device
-        return
 
     def count_parameters_size(self):
-        mods = list(self.model.modules())
         sizes = []
-        
-        for i in range(1,len(mods)):
-            m = mods[i]
-            p = list(m.parameters())
-            for j in range(len(p)):
-                sizes.append(np.array(p[j].size()))
-
+        for m in self.model.modules():
+            for p in m.parameters():
+                sizes.append(np.array(p.size()))
         self.param_sizes = sizes
-        return
 
     def count_output_size(self):
         input_ = Variable(torch.FloatTensor(*self.input_size))
@@ -30,31 +26,22 @@ class RAMSizeCounter:
         with torch.no_grad():
             out = self.model(input_.to(self.device))
             out_sizes.append(np.array(out.size()))
-
         self.out_sizes = out_sizes
-        return
 
     def count_parameters_ram(self):
-        total_bits = 0
-        for i in range(len(self.param_sizes)):
-            s = self.param_sizes[i]
-            bits = np.prod(np.array(s)) * self.bits
-            total_bits += bits
-        self.param_bits = total_bits
-        return
+        bits = 0
+        for s in self.param_sizes:
+            bits += np.prod(np.array(s)) * self.bits
+        self.param_bits = bits
 
     def count_passes_ram(self):
-        total_bits = 0
-        for i in range(len(self.out_sizes)):
-            s = self.out_sizes[i]
-            bits = np.prod(np.array(s)) * self.bits
-            total_bits += bits
-        self.forward_backward_bits = (total_bits * 2)
-        return
+        bits = 0
+        for s in self.out_sizes:
+            bits += np.prod(np.array(s)) * self.bits
+        self.forward_backward_bits = 2 * bits
 
     def count_input_ram(self):
         self.input_bits = np.prod(np.array(self.input_size)) * self.bits
-        return
 
     def count_megabytes(self):
         self.count_parameters_size()
@@ -65,5 +52,5 @@ class RAMSizeCounter:
         result = self.param_bits + self.forward_backward_bits + self.input_bits
 
         megabytes = (result / 8) / (1024 ** 2)
-        print("Model size in megabytes:", megabytes)
+        print(f"Model size is {megabytes} MB")
         return megabytes
